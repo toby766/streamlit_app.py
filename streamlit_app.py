57dada26bcf396465a1848085185df9babdcc195
toby766 <98780108+toby766@users.noreply.github.com>
2022-12-27 21:33:14 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,23 @@
 import streamlit as st
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
 
+# load the data
+df = pd.read_csv("admissions_data.csv")
+
+# preprocess the data
+df = df.dropna()
+df = pd.get_dummies(df)
+
+# split the data into training and test sets
+X = df.drop("Chance of Admit", axis=1)
+y = df["Chance of Admit"]
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+# train the model
+model = LinearRegression()
+model.fit(X_train, y_train)
 # create the Streamlit app
 st.title("Admission Prediction App")
 
@@ -19,4 +37,23 @@ if research_experience == "Yes":
 else:
   research_experience = 0
 
+
+input_data = {
+    "GRE Score": gre_score,
+    "TOEFL Score": toefl_score,
+    "University Rating": university_rating,
+    "SOP Strength": sop_strength,
+    "LOR Strength": lor_strength,
+    "Undergraduate GPA": undergraduate_gpa,
+    "Research Experience_Yes": 1 if research_experience == "Yes" else 0,
+    "Research Experience_No": 1 if research_experience == "No" else 0,
+}
+
+input_data = pd.DataFrame([input_data])
+
+# make the prediction
+prediction = model.predict(input_data)[0]
+
+# display the prediction
+st.write(f"The predicted chance of admission is: {prediction:.2f}")
 
